9c4413222641553d5eca659730de87ddcebee5a8
Agata872 <2310123801@qq.com>
2025-06-16 12:45:46 +0200

    edit the code

--- client/combingTxRx.py
+++ client/combingTxRx.py
@@ -69,7 +69,7 @@ def main():
     tx_target = f"{global_user}@{tx_ip}"
 
     TX_SCRIPT_PATH = "~/Techtile_Channel_Measurement/client/Tx.py"
-    RX_SCRIPT_PATH = "~/Techtile_Channel_Measurement/client/Rx.py"
+    RX_SCRIPT_PATH = "~/Techtile_Channel_Measurement/client/Tx.py"
 
     print(f"🚀 启动发射端 {TX_NAME} ({tx_target}) ...")
     tx_thread = threading.Thread(target=run_remote_script, args=(tx_target, TX_SCRIPT_PATH))
